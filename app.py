0aa1accd159ae35b95bdd4dd4b93c309ac2fc950
Pallavi Anil Sindkar <83487183+Pallavi-S-02@users.noreply.github.com>
2024-02-23 22:03:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from chatbot import get_base64
+from chatbot import load_pdf_file, create_chunks_and_embeddings, load_llm, create_prompt_template
+
+def main():
+    st.set_page_config(page_title="Chat with Pallavi")
+    st.header("*:White[Feel free to know more about Pallavi]*")
+    st.markdown("<h5 style='color: #0000CD;'>You can ask me a question and my personal AI will answer you</h5>", unsafe_allow_html=True)
+    
+    user_question = st.text_input("Type your question below", "")
+    
+    def set_background(png_file):
+        bin_str = get_base64(png_file)
+        page_bg_img = '''
+        <style>
+        .stApp {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        }
+        .stApp {
+            background-color: rgba(255, 255, 255, 0.8); /* Optional: Add a semi-transparent white background to make text more readable */
+        }
+        .stMarkdown {
+            color: black; /* Set default text color to black */
+            font-weight: bold; /* Make text bold */
+        }
+        </style>
+        ''' % bin_str
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+    
+    set_background('image.jfif')
+    
+    #for input text box
+    st.markdown(
+    """
+    <style>
+    /* Change the border color and box shadow color */
+    input[type="text"] {
+        border: 1px solid #FFB6C1 !important;
+        box-shadow: 0 0 0 1px #FFB6C1 !important;
+        background-color: #FFB6C1 !important; /* Baby pink color as fallback */
+        color: black !important; /* Text color */
+    }
+
+    /* Change the text color of the placeholder */
+    input[type="text"]::placeholder {
+        color: #FFB6C1 !important; /* Placeholder text color */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+    #for response to be in box
+    st.markdown(
+        """
+        <style>
+        .response {
+            color: black;
+            background-color: #E6E6FA;
+            padding: 10px;
+            border-radius: 5px;
+            margin-top: -40px; /* Negative margin to pull the box up */
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    if user_question:
+        # Wrap the entire app content in a div
+        with st.markdown("<div>", unsafe_allow_html=True):
+            pdf_data = load_pdf_file('bio.pdf')
+            Ensemble_Retriever = create_chunks_and_embeddings(pdf_data) 
+            llm_model = load_llm()
+            chain = create_prompt_template(Ensemble_Retriever, llm_model)
+            response = chain.invoke(user_question)
+            response_with_prefix = f"<span style='font-weight:bold'>Answer:</span> <span style='font-weight:normal'>{response}</span>"
+            st.write(f"<div class='response'>{response_with_prefix}</div>", unsafe_allow_html=True)
+
+    st.markdown(
+    """
+    <br><br><br>
+    <div style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: #FFFAFA; text-align: center; padding: 10px;">
+        Pallavi Sindkar <br>
+        <a href="https://www.linkedin.com/in/pallavi-sindkar-83b583203" target="_blank">LinkedIn</a> |
+        <a href="https://github.com/Pallavi-S-02" target="_blank">GitHub</a>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
+
+    # Close the outer div
+    st.markdown("</div>", unsafe_allow_html=True)
+
+main()
+
+
---
+++ chatbot.py
@@ -0,0 +1,80 @@
+#Google Embeddings, Gemini Pro, BM25 RAG with Ensemble.
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.retrievers import BM25Retriever
+from langchain.retrievers import EnsembleRetriever
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import base64
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def load_pdf_file(pdf): #pdf=bio.pdf
+    data_file = UnstructuredPDFLoader(pdf)
+    docs = data_file.load()
+    return docs
+
+def create_chunks_and_embeddings(docs):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=800,chunk_overlap=100)
+    chunks = splitter.split_documents(docs)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vectorstore = Chroma.from_documents(chunks, embeddings)
+    vectorstore_retreiver = vectorstore.as_retriever(search_kwargs={"k": 3})    
+    keyword_retriever = BM25Retriever.from_documents(chunks)
+    keyword_retriever.k =  3    
+    ensemble_retriever = EnsembleRetriever(retrievers=[vectorstore_retreiver,keyword_retriever],weights=[0.5, 0.5])
+    return ensemble_retriever
+
+def load_llm():
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.6)
+    return llm
+
+def create_prompt_template(ensemble_retriever, llm):
+    template = """
+    <|system|>>
+    You are a helpful AI Assistant that follows instructions extremely well.
+    Use the following context to answer user question.
+    The following context is all about a person named 'Pallavi'. Users should feel that they are talking with Pallavi not an AI bot.
+    
+    Think step by step before answering the question. You will get a $100 tip if you provide correct answer. 
+    If the answer is not in provided context just say, "I am sorry, I don't have permission to provide you this information", don't provide the wrong answer.
+    
+    CONTEXT: {context}
+    </s>
+    <|user|>
+    {query}
+    </s>
+    <|assistant|>
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    output_parser = StrOutputParser()
+    
+    chain = (
+        {"context": ensemble_retriever, "query": RunnablePassthrough()}
+        | prompt
+        | llm
+        | output_parser
+    )
+
+    return chain
+
+def get_base64_encoded_image(image_filename):
+    with open(image_filename, 'rb') as f:
+        encoded_string = base64.b64encode(f.read()).decode()
+    return f"data:image/png;base64,{encoded_string}"
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
