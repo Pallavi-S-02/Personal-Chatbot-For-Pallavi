fedfdbb2cc3dc3288070e69c2278c39c1ca3ef2f
Pallavi Anil Sindkar <83487183+Pallavi-S-02@users.noreply.github.com>
2024-03-05 20:47:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from chatbot import get_base64
 from chatbot import load_pdf_file, create_chunks_and_embeddings, load_llm, create_prompt_template
